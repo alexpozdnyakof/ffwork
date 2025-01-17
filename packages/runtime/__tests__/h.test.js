ff1185b89a2ff2aab6313dcc7e951ab9cfc2723e
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-01-17 16:19:13 +0700

    test(runtime): add virtual dom

---
+++ packages/runtime/__tests__/h.test.js
@@ -0,0 +1,115 @@
+import { expect, test } from "vitest";
+import { h, hFragment } from "../src/h";
+
+test("should create element", () => {
+  expect(h("div")).toEqual({
+    tag: "div", 
+    type: "element", 
+    props: {}, 
+    children: []
+  });
+})
+
+test("should add props and childrens to element", () => {
+  expect(
+    h(
+      "form", 
+      { 
+        class: "login-form", 
+        action: "login"
+      }, 
+      [
+        h(
+          "input", 
+          {
+            type: "text", 
+            name: "user"
+          }
+        )
+      ]
+    )
+  ).toEqual({
+    tag: "form", 
+    type: "element", 
+    props: {
+      class: "login-form",
+      action: "login"
+    }, 
+    children: [{
+      tag: "input",
+      type: "element",
+      props: {
+        type: "text",
+        name: "user"
+      },
+      children: []
+    }]
+  });
+})
+
+
+test("should remove nulled childrens", () => {
+  expect(h("div", {}, [null])).toEqual({
+      tag: "div", 
+      type: "element", 
+      props: {}, 
+      children: []
+  });
+})
+
+
+test("should map raw string children to text element", () => {
+  expect(h("div", {}, ["str"])).toEqual({
+      tag: "div", 
+      type: "element", 
+      props: {}, 
+      children: [{
+        value: "str", 
+        type: "text"
+      }]
+  });
+})
+
+test("should create fragment with props and childrens", () => {
+  expect(
+    hFragment( 
+      [
+        h(
+          "input", 
+          {
+            type: "text", 
+            name: "user"
+          }
+        ),
+        h(
+          "input", 
+          {
+            type: "password", 
+            name: "pass"
+          }
+        )
+      ]
+    )
+  ).toEqual({
+    type: "fragment", 
+    children: [{
+      tag: "input",
+      type: "element",
+      props: {
+        type: "text",
+        name: "user"
+      },
+      children: []
+    },
+    {
+      tag: "input",
+      type: "element",
+      props: {
+        type: "password",
+        name: "pass"
+      },
+      children: []
+    }]
+  });
+})
+
