e2dbbd075ee5f97b3693ff938aa6e80b3b79494b
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-01-20 18:11:11 +0700

    feat(runtime): add dom unmount fn

---
+++ packages/runtime/__tests__/unmount.test.js
@@ -0,0 +1,70 @@
+import { expect, test, beforeEach, vi } from "vitest";
+import { mount } from "../src/mount";
+import { unmount } from "../src/unmount";
+import { h, hString, hFragment } from "../src/h";
+
+beforeEach(() => {
+  vi.unstubAllGlobals();
+  document.body.innerHTML = "";
+})
+
+test("should remove the mounted element", () => {
+  const node = h("button");
+  mount(node, document.body);
+  expect(document.body.innerHTML).toBe("<button></button>");
+  unmount(node);
+  expect(document.body.innerHTML).toBe("");
+})
+
+test("should remove the reference of mounted element", () => {
+  const node = h("button");
+  mount(node, document.body);
+  expect(node.el).toBeDefined();
+  unmount(node);
+  expect(node.el).not.toBeDefined();
+})
+
+
+test("should remove the reference of mounted text", () => {
+  const node = hString("text");
+  mount(node, document.body);
+  expect(node.el).toBeDefined();
+  unmount(node);
+  expect(node.el).not.toBeDefined();
+})
+
+test("should remove the mounted text", () => {
+  const node = hString("text");
+  mount(node, document.body);
+  expect(document.body.innerHTML).toBe("text");
+  unmount(node);
+  expect(document.body.innerHTML).toBe("");
+})
+
+
+test("should remove the reference of mounted fragment", () => {
+  const node = hFragment([h("button"), hString("text")]);
+  mount(node, document.body);
+  expect(node.el).toBeDefined();
+  unmount(node);
+  expect(node.el).not.toBeDefined();
+})
+
+test("should remove the mounted fragment", () => {
+  const node = hFragment([h("button"), hString("text")]);
+  mount(node, document.body);
+  expect(document.body.innerHTML).toBe("<button></button>text")
+  unmount(node);
+  expect(document.body.innerHTML).toBe("");
+})
+
+test("should remove the mounted fragment and save an other elements in root", () => {
+  const fragment = hFragment([h("button"), hString("text")]);
+  const element = h("div");
+  mount(fragment, document.body);
+  mount(element, document.body);
+  expect(document.body.innerHTML).toBe("<button></button>text<div></div>")
+  unmount(fragment);
+  expect(document.body.innerHTML).toBe("<div></div>");
+})
+
---
+++ packages/runtime/src/unmount.js
@@ -0,0 +1,45 @@
+import { DOM_TYPES } from "./h";
+import { removeEventListeners } from "./events";
+
+export function unmount(vdom){
+  const { type } = vdom;
+
+  switch(type) {
+    case DOM_TYPES.TEXT: {
+      removeTextNode(vdom);
+      break;
+    }
+    case DOM_TYPES.ELEMENT: {
+      removeElementNode(vdom);
+      break;
+    }
+    case DOM_TYPES.FRAGMENT: {
+      removeFragmentNode(vdom);
+      break;
+    }
+    default: throw new Error(`Unexpected type ${type}`);
+  }
+
+  delete vdom.el;
+}
+
+function removeTextNode(v) {
+  const { el: element } = v;
+  element.remove();
+}
+function removeElementNode(v){
+  const { el: element, children, listeners } = v;
+
+  element.remove();
+  children.forEach(unmount);
+
+  if (listeners) {
+    removeEventListeners(listeners, element);
+    delete v.listeners;
+  }
+}
+function removeFragmentNode(v){
+  const { children } = v;
+  children.forEach(unmount);
+}
+
