84813beeccb44f9976afe4fbd8301a69ed669404
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-04-02 16:39:11 +0700

    refactor(runtime): using patching instead unmount

--- packages/runtime/src/create-app.js
+++ packages/runtime/src/create-app.js
@@ -1,43 +1,47 @@
 import { unmount } from "./unmount";
 import { mount } from "./mount";
 import { dispatcher } from "./dispatcher";
+import { patch } from "./patch";
 
 export function createApp({ state, view, reducers = {} }) {
   let root = null;
   let vdom = null;
   let _state = state;
+  let isMounted = false;
   const _dispatcher = dispatcher();
-  
-  const emit = (eventName, payload) => { _dispatcher.dispatch(eventName, payload) };
-  
-  const render = () => {
-    if(vdom) {
-      unmount(vdom);
-    } 
 
-    vdom = view(_state, emit);
-    mount(vdom, root);
-  }
-  
+  const emit = (eventName, payload) => {
+    _dispatcher.dispatch(eventName, payload);
+  };
+
+  const render = () => {
+    const next = view(_state, emit);
+    vdom = patch(vdom, next, root);
+  };
+
   const subscriptions = [_dispatcher.after(render)];
 
   for (const actionName in reducers) {
     const reducer = reducers[actionName];
 
-    const subscribe = _dispatcher.subscribe(actionName, (payload) => { _state = reducer(_state, payload) });
+    const subscribe = _dispatcher.subscribe(actionName, (payload) => {
+      _state = reducer(_state, payload);
+    });
     subscriptions.push(subscribe);
   }
 
   return {
     mount(_root) {
+      if (isMounted) throw new Error("The app is already mounted");
       root = _root;
-      render();
+      vdom = view(state, emit);
+      mount(vdom, root);
     },
     unmount() {
       unmount(vdom);
       vdom = null;
-      subscriptions.forEach(unsubscribe => unsubscribe());
-    }
-  }
+      subscriptions.forEach((unsubscribe) => unsubscribe());
+      isMounted = false;
+    },
+  };
 }
-
