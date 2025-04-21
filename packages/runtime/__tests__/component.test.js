5926fb06651eb2cd4fdc2d3a60df4e83f08b2552
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-04-21 18:10:27 +0700

    test(runtime): add subcomponents support

--- packages/runtime/__tests__/component.test.js
+++ packages/runtime/__tests__/component.test.js
@@ -117,4 +117,60 @@ describe("component", () => {
 
     expect(document.body.innerHTML).toBe(`<button>1</button>`);
   });
+
+  it("should render the subcomponents", () => {
+    const ListItem = defineComponent({
+      render() {
+        return h("li", {}, [`${this.props.item}`]);
+      },
+    });
+
+    const List = defineComponent({
+      render() {
+        return h(
+          "ul",
+          {},
+          this.props.items.map((item) => h(ListItem, { item }))
+        );
+      },
+    });
+
+    const list = new List({ items: [1, 2, 3] });
+    list.mount(document.body);
+
+    expect(document.body.innerHTML).toBe(
+      "<ul><li>1</li><li>2</li><li>3</li></ul>"
+    );
+  });
+  it("should unmount the component", () => {
+    const Button = defineComponent({
+      render() {
+        return h("button", {}, ["submit"]);
+      },
+    });
+
+    const button = new Button();
+    button.mount(document.body);
+
+    expect(document.body.innerHTML).toBe("<button>submit</button>");
+
+    button.unmount();
+    expect(document.body.innerHTML).toBe("");
+  });
+
+  it("should update props", () => {
+    const Count = defineComponent({
+      render() {
+        return h("div", {}, [`${this.props.count}`]);
+      },
+    });
+
+    const count = new Count({ count: 0 });
+    count.mount(document.body);
+
+    expect(document.body.innerHTML).toBe("<div>0</div>");
+
+    count.updateProps({ count: 1 });
+    expect(document.body.innerHTML).toBe("<div>1</div>");
+  });
 });
