672dfb6d9f47cc0837fc50a41451d49246ee066c
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-05-13 15:07:58 +0700

    refactor(runtime): add component support for createapp

--- packages/runtime/__tests__/create-app.test.js
+++ packages/runtime/__tests__/create-app.test.js
@@ -1,32 +1,45 @@
 import { expect, test, afterEach, beforeEach, vi } from "vitest";
 import { createApp } from "../src/create-app";
 import { h, hString } from "../src/h";
-
+import { defineComponent } from "../src/component";
 let app;
 
-beforeEach(() =>{
+beforeEach(() => {
   vi.unstubAllGlobals();
   document.body.innerHTML = "";
-})
+});
 
 afterEach(() => {
   if (app && "unmount" in app) app.unmount();
   app = undefined;
-})
-
-test("it should mount app to the document", () => { 
-  app = createApp({
-    view: (state, emit) => h("button", {}, [hString("submit")])
-  });
-  app.mount(document.body);
-
-  expect(document.body.innerHTML).toBe("<button>submit</button>") 
 });
 
-test("it should unmount app from the document", () => { 
-  app = createApp({
-    view: (state, emit) => h("button", {}, [hString("submit")])
-  });
+const RootComponent = defineComponent({
+  render() {
+    const { text } = this.props;
+    return h("button", { type: "submit" }, [text ?? hString("submit")]);
+  },
+});
+
+test("it should mount app to the document", () => {
+  app = createApp(RootComponent);
+  app.mount(document.body);
+
+  expect(document.body.innerHTML).toBe(`<button type="submit">submit</button>`);
+});
+
+test("it should throw an error when mount app twice", () => {
+  app = createApp(RootComponent);
+  app.mount(document.body);
+
+  expect(document.body.innerHTML).toBe(`<button type="submit">submit</button>`);
+  expect(() => app.mount(document.body)).toThrowError(
+    "The app is already mounted"
+  );
+});
+
+test("it should unmount app from the document", () => {
+  app = createApp(RootComponent);
   app.mount(document.body);
   expect(document.body.innerHTML).toBeDefined();
 
@@ -35,49 +48,9 @@ test("it should unmount app from the document", () => {
   app = undefined;
 });
 
-test("it should render the state", () => { 
-  app = createApp({
-    state: 0,
-    view: (state => hString(state))
-  });
-  app.mount(document.body);
+test("it should throw an error when unmount not mounted app", () => {
+  app = createApp(RootComponent);
 
-  expect(document.body.innerHTML).toBe("0");
+  expect(() => app.unmount()).toThrowError("The app is not mounted");
+  app = undefined;
 });
-
-test("it should attach the handler and dispatch the command", () => {
-  const handler = vi.fn();
-  app = createApp({
-    state: 0,
-    reducers: {
-      add: handler,
-    },
-    view: (state, emit) => h("button", { on: { click(){ emit("add", 1) }}}, [hString(state)])
-  });
-
-  app.mount(document.body);
-
-  const button = document.body.querySelector("button");
-  button.click();
-
-  expect(handler).toHaveBeenCalledTimes(1); 
-  expect(handler).toHaveBeenCalledWith(0, 1);
-});
-
-test("it should rerender the view when the state is changed", () => { 
-  app = createApp({
-    state: 0,
-    reducers: {
-      add: (state, count) => state + count,
-    },
-    view: (state, emit) => h("button", { on: { click(){ emit("add", 1) }}}, [hString(state)])
-  });
-  app.mount(document.body);
-  expect(document.body.innerHTML).toBe("<button>0</button>");
-
-  const button = document.body.querySelector("button");
-  button.click();
-  
-  expect(document.body.innerHTML).toBe("<button>1</button>");  
-});
-
--- packages/runtime/src/create-app.js
+++ packages/runtime/src/create-app.js
@@ -1,47 +1,30 @@
 import { unmount } from "./unmount";
 import { mount } from "./mount";
-import { dispatcher } from "./dispatcher";
-import { patch } from "./patch";
-
-export function createApp({ state, view, reducers = {} }) {
+import { h } from "./h";
+export function createApp(RootComponent, props = {}) {
   let root = null;
   let vdom = null;
-  let _state = state;
   let isMounted = false;
-  const _dispatcher = dispatcher();
 
-  const emit = (eventName, payload) => {
-    _dispatcher.dispatch(eventName, payload);
-  };
-
-  const render = () => {
-    const next = view(_state, emit);
-    vdom = patch(vdom, next, root);
-  };
-
-  const subscriptions = [_dispatcher.after(render)];
-
-  for (const actionName in reducers) {
-    const reducer = reducers[actionName];
-
-    const subscribe = _dispatcher.subscribe(actionName, (payload) => {
-      _state = reducer(_state, payload);
-    });
-    subscriptions.push(subscribe);
+  function reset() {
+    root = null;
+    isMounted = false;
+    vdom = null;
   }
 
   return {
     mount(_root) {
       if (isMounted) throw new Error("The app is already mounted");
       root = _root;
-      vdom = view(state, emit);
+      vdom = h(RootComponent, props);
       mount(vdom, root);
+
+      isMounted = true;
     },
     unmount() {
+      if (!isMounted) throw new Error("The app is not mounted");
       unmount(vdom);
-      vdom = null;
-      subscriptions.forEach((unsubscribe) => unsubscribe());
-      isMounted = false;
+      reset();
     },
   };
 }
